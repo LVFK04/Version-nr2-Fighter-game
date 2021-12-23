b7652e4b936dbc00d7a2de0bb1ae868aeb27344c
Deltagare <Deltagare@5CG5443XCH>
2021-12-23 21:35:30 +0100

    Add project files.

---
+++ FighterPlayer.cpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "FighterPlayer.h"
+using namespace std;
+
+void Fighter::attack(Fighter tar) {
+
+};
+
+void Fighter::defend(unsigned int atkPower) {
+
+};
+
+void Fighter::rest() {
+	health += 10;
+	stamina += 15;
+};
